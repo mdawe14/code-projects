c242bb0197843e96d8d91786136db9c286431212
mdawe14 <mdawe1@myseneca.ca>
2017-04-12 19:05:53 -0400

    Add files via upload

---
+++ Java/test_concatenation_methods/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created by melan on 2017-02-14 for BIF812 Assignment 1.
+ * purpose:  To understand and implement basic Java classes and
+ * methods and to test the effectiveness of different concatenation methods.
+ * /*I declare that the attached assignment is my own work in accordance with Seneca Academic
+ Policy. No part of this assignment has been copied manually or electronically from any other
+ source (including web sites) or distributed to other students.
+ Name: Melanie Dawe Student ID: 129089157
+ */
+public class Test extends MiniGenBankSeq {
+    public static void main(String[] args) {
+        SequenceLoader sLoader = new SequenceLoader();
+        System.out.println(sLoader.testConcatenate(10));
+    }
+}
